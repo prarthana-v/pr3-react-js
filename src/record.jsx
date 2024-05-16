2a85f29ed7de29e6371883ec64edb6dd04c5f024
prarthana-v <p.d.vaghani1729@gmail.com>
2024-05-16 15:29:21 +0530

    event-Handler

--- src/record.jsx
+++ src/record.jsx
@@ -2,17 +2,16 @@ import React from 'react'
 import './assets/style.css'
 const Record = ({ product, category, filterbtn }) => {
   return (
-    <div>
-      <div className="container bg-light">
+    <div className='body'>
+      <div className="container">
         <div className="row " >
-          <h1 align="center" className='my-3 text-primary'>App store</h1>
+          <h1 align="center" className='my-3 text-light bolder'>App store</h1>
 
           <div className='pt-4 ' align="center">
             {
               category.map((item) => {
-                // console.log(item);
                 return (
-                  <button key={item.id} className='btn btn-primary mx-3' onClick={() => filterbtn(item.category)}>{item.category}</button>
+                  <button key={item.id} className='btn btn-secondary mx-3' onClick={() => filterbtn(item.category)}>{item.category}</button>
                 )
               })
             }
@@ -29,8 +28,7 @@ const Record = ({ product, category, filterbtn }) => {
                       </div>
                       <div className="card-body">
                         <h5 className="card-title">{item.name}</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" className="btn btn-primary">ADD</a>
+                        <a href="#" className="btn btn-secondary">ADD</a>
                       </div>
                     </div>
                   </div>
